6e14cb6ed713891013b32ceb29685b4eb671722a
Gabrielyn Strujak <107216619+strujakgabrielyn@users.noreply.github.com>
2022-11-18 21:28:48 -0300

    acabou natal

--- src/App.js
+++ src/App.js
@@ -4,15 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
-      <p>dingo bell</p>
-      <p>dingo bell</p>
-      <p>dingo bell</p>
-      <p>dingo bell</p>
-      <p>dingo bell</p>
-      <p>dingo bell</p>
-      <p>dingo bell</p>
-      <p>dingo bell</p>
+    
 
     </div>
   );
