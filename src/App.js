038eaef1f35ad6ccfcc9173ce3d9c60cb0a8bc84
samuraizinhu <douglasdsr@outlook.com>
2022-11-18 21:24:50 -0300

    att natalina

--- src/App.js
+++ src/App.js
@@ -5,7 +5,14 @@ function App() {
   return (
     <div className="App">
       
-      <p>aaaaaTESTE</p>
+      <p>dingo bell</p>
+      <p>dingo bell</p>
+      <p>dingo bell</p>
+      <p>dingo bell</p>
+      <p>dingo bell</p>
+      <p>dingo bell</p>
+      <p>dingo bell</p>
+      <p>dingo bell</p>
 
     </div>
   );
