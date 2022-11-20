56c37c0d39e2fb436d0f258092da09199f654307
46ice <ice.46.frost@gmail.com>
2022-11-19 22:20:33 -0300

    frost_1.2

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,15 @@ import { useState } from "react";
 import "react-bootstrap";
 import "./App.css";
 import Header from "./components/Header";
-import Home from "./components/Home";
+import Home from "./Pages/Home";
 import Footer from "./components/Footer";
 import Suporte from "./Pages/Suporte";
 function App() {
   return (
     <div className="App">
-      <Header />
 
       <Home />
 
-      <Footer />
     </div>
   );
 }
